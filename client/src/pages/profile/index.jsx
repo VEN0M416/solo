0f5918f205cfb319f42057ad281774c97c2ab9ff
Amon Karimov <amonullo.k.02@mail.ru>
2024-11-15 00:38:10 +0300

    add the change user data

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,26 +1,106 @@
 import Header from "../../main-modules/header";
 import { useCookies } from 'react-cookie';
+import { useState, useEffect } from 'react';
+import { api } from "../../main-modules/core/api";
 import './index.css';
 
 export const Profile = () => {
-    const [cookies,] = useCookies(['username']);
+    const [cookies, setCookie] = useCookies(['username', 'sessionId']);
+    const [isEditing, setIsEditing] = useState(false);
+    const [login, setUsername] = useState(cookies.username || '');
+    const [email, setEmail] = useState(''); // Initial state will be empty
+    const user_id = cookies.sessionId; // Assuming sessionId is the user ID
+
+    const [usernameAlreadyTaken, setUsernameAlreadeTaken] = useState(false);
+
+    // Fetch user data on component mount
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const response = await api.get(`/user/${user_id}`);
+                setUsername(response.data.login);
+                (response.data.email != null) ? setEmail(response.data.email) : setEmail("example@exmp.ru");
+            } catch (error) {
+                console.log('Error fetching user data:', error.message);
+            }
+        };
+        fetchUserData();
+    }, [user_id]);
+
+    // Save changes to user data
+    const handleSave = async () => {
+        try {
+            const response = await api.put(`/edit_data`, {
+                user_id,
+                login,
+                email
+            });
+
+            if (response.data.answer === 'update successful') {
+                // Update cookies if username changed
+                setCookie('username', login, { path: '/', sameSite: 'Lax' });
+                console.log('update successful');
+                /* console.log(response.data); */
+                setIsEditing(false); // Exit editing mode
+                setUsernameAlreadeTaken(false);
+            } else if (response.data.answer === 'username already taken') {
+                console.log('username already taken');
+                setUsernameAlreadeTaken(true);
+            } else {
+                console.log('Failed to update user data');
+            }
+        } catch (error) {
+            console.log('Error updating user data:', error.message);
+        }
+    };
 
     return (
         <div className='Profile'>
             <Header />
             <div className="Body-Profile">
                 <div className="profile-container">
-                    <h1>Добро пожаловать, {cookies.username}!</h1>
+                    <h1>Добро пожаловать, {login}!</h1>
                     <p className="profile-intro">Это страница вашего личного профиля, где вы можете управлять настройками своей учетной записи и просматривать свою активность.</p>
 
                     <div className="profile-info">
-                        <h2>Your Details</h2>
-                        <p><strong>Username:</strong> {cookies.username}</p>
-                        <p><strong>Email:</strong> user@example.com</p> {/* Пример данных, можно заменить на реальные */}
-                    </div>
+                        <h2 className="Head">Ваши данные</h2>
 
+                        {isEditing ? (
+                            <>
+                                <label>
+                                    <strong>Имя:</strong>
+                                    <input
+                                        type="text"
+                                        value={login}
+                                        onChange={(e) => setUsername(e.target.value)}
+                                    />
+                                </label>
+                                <label>
+                                    <strong>Почта:</strong>
+                                    <input
+                                        type="email"
+                                        value={email}
+                                        onChange={(e) => setEmail(e.target.value)}
+                                    />
+                                </label>
+                            </>
+                        ) : (
+                            <>
+                                <h2><strong>Имя:</strong> {login}</h2>
+                                <h2><strong>Почта:</strong> {email}</h2>
+                            </>
+                        )}
+                    </div>
+                    <p className="warning">{usernameAlreadyTaken && 'Пользователь с таким именем уже существует!'}</p>
                     <div className="profile-actions">
-                        <button className="edit-profile-button">Изменить</button>
+                        {isEditing ? (
+                            <>
+                                <button className="save-profile-button" onClick={handleSave}>Сохранить</button>
+                                <button className="cancel-profile-button" onClick={() => setIsEditing(false)}>Отмена</button>
+                            </>
+                        ) : (
+                            <button className="edit-profile-button" onClick={() => setIsEditing(true)}>Изменить</button>
+                        )}
                     </div>
                 </div>
             </div>
--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ app.post('/authorization', async (req, res) => {
         const signup = await pool.query('SELECT user_id, password FROM users WHERE login = $1',
             [login]
         );
- 
+
         if (signup.rowCount !== 0) {    // проверка на существование
             const newPassword = signup.rows[0].password;
 
@@ -59,6 +59,54 @@ app.post('/authorization', async (req, res) => {
         console.log(error.message);
     }
 })
+
+// get the information by id
+app.get('/user/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const user = await pool.query('SELECT login, email FROM users WHERE user_id = $1', [id]);
+        if (user.rowCount > 0) {
+            res.json(user.rows[0]);
+        } else {
+            res.status(404).json({ error: "User not found" });
+        }
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
+// update user data
+app.put('/edit_data', async (req, res) => {
+    const { user_id, login, email } = req.body;
+
+    try {
+        // Проверка на существование пользователя с новым логином (кроме текущего пользователя)
+        const existingUser = await pool.query(
+            'SELECT login FROM users WHERE login = $1 AND user_id != $2',
+            [login, user_id]
+        );
+
+        if (existingUser.rowCount > 0) {
+            return res.json({ answer: "username already taken" });
+        }
+
+        // Обновление данных пользователя
+        await pool.query(
+            'UPDATE users SET login = $1, email = $2 WHERE user_id = $3',
+            [login, email, user_id]
+        );
+
+        // Отправка ответа клиенту
+        res.json({ answer: "update successful" });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ answer: "server error" });
+    }
+});
+
+
+
 
 
 // start server
